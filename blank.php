bbf1ac8242b975cbcfd879b81a1a40d125d2c942
leodanuarta <leo.535210024@stu.untar.ac.id>
2024-02-27 22:20:59 +0700

    first commit

---
+++ blank.php
@@ -0,0 +1,22 @@
+<?php
+    $title = "Blank Page";
+    include_once('./props/header.php');
+?>
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+
+
+<?php
+    include_once('./props/footer.php');
+?>
